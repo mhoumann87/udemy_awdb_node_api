55b706a20bde7748aa6e9f97fc3c02b5ac013b72
Michael Houmann <michael@michael-h.dk>
2019-01-16 09:54:29 +0100

    Refactor the routes, create helper file for todos

--- routes/todos.js
+++ routes/todos.js
@@ -1,35 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+const helpers = require('../helpers/todos');
 
-router.get('/', (req, res) => {
-  db.Todo.find()
-    .then((todos) => res.json(todos))
-    .catch((err) => res.send(err));
-});
+router.route('/')
+  .get(helpers.getAllTodos)
+  .post(helpers.createTodo)
 
-router.get('/:todoId', (req, res) => {
-  db.Todo.findById(req.params.todoId)
-    .then((foundTodo) => res.json(foundTodo))
-    .catch((err) => res.send(err));
-});
-
-router.post('/', (req, res) => {
-  db.Todo.create(req.body)
-    .then((newTodo) => res.status(201).json(newTodo))
-    .catch((err) => res.send(err));
-});
-
-router.put('/:todoId', (req, res) => {
-  db.Todo.findByIdAndUpdate({ _id: req.params.todoId }, req.body, { new: true })
-    .then((todo) => res.json(todo))
-    .catch((err) => res.send(err));
-});
-
-router.delete('/:todoId', (req, res) => {
-  db.Todo.remove({ _id: req.params.todoId })
-    .then(() => res.json({ message: "Todo is deleted" }))
-    .catch((err) => res.send(err));
-});
+router.route('/:todoId')
+  .get(helpers.getOneTodo)
+  .put(helpers.updateTodo)
+  .delete(helpers.deleteTodo)
 
 module.exports = router;
