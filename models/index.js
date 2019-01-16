6e64f3671a35500b6f66d47ec4eb93c8be042c7e
Michael Houmann <michael@michael-h.dk>
2019-01-16 09:30:02 +0100

    Add API routes to the app

--- models/index.js
+++ models/index.js
@@ -3,13 +3,6 @@ const mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.connect('mongodb://localhost/todo-api', { useNewUrlParser: true });
 
-// test connection
-/* var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', function () {
-  console.log('connected');
-}) */
-
 mongoose.Promise = Promise;
 
-module.exports = require('./todo');
+module.exports.Todo = require('./todo');
--- routes/todos.js
+++ routes/todos.js
@@ -2,21 +2,34 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
-
-
 router.get('/', (req, res) => {
   db.Todo.find()
-    .then((todos) => {
-      res.json(todos);
-    })
+    .then((todos) => res.json(todos))
     .catch((err) => res.send(err));
 });
 
-router.post('/', function (req, res) {
-  db.Todo.create(req.body)
-    .then((newTodo) => {
-      res.json(newTodo)
-    })
+router.get('/:todoId', (req, res) => {
+  db.Todo.findById(req.params.todoId)
+    .then((foundTodo) => res.json(foundTodo))
     .catch((err) => res.send(err));
-})
+});
+
+router.post('/', (req, res) => {
+  db.Todo.create(req.body)
+    .then((newTodo) => res.status(201).json(newTodo))
+    .catch((err) => res.send(err));
+});
+
+router.put('/:todoId', (req, res) => {
+  db.Todo.findByIdAndUpdate({ _id: req.params.todoId }, req.body, { new: true })
+    .then((todo) => res.json(todo))
+    .catch((err) => res.send(err));
+});
+
+router.delete('/:todoId', (req, res) => {
+  db.Todo.remove({ _id: req.params.todoId })
+    .then(() => res.json({ message: "Todo is deleted" }))
+    .catch((err) => res.send(err));
+});
+
 module.exports = router;
